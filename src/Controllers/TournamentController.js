e83c358fefecc9d4ad76090b3b0fec985153d920
sk-jo <gajeong01@gmail.com>
2024-10-14 13:21:45 +0900

    Add tournamentRanking

--- src/Controllers/TournamentController.js
+++ src/Controllers/TournamentController.js
@@ -1,6 +1,20 @@
 const tournamentService = require('../Services/TournamentService');
 
 const tournamentController = {
+    async getTournaments(req, res) {
+        const tournaments = await tournamentService.getTournaments();
+        res.json(tournaments);
+    },
+
+    async getTournamentCounts(req, res) {
+        const { tournamentId } = req.params;
+        console.log('request getTournamentCounts:', tournamentId);
+
+        const counts = await tournamentService.getTournamentCountOptions(tournamentId);
+
+        res.json(counts);
+    },
+
     async getTournamentImages(req, res) {
         const { tournamentId, count } = req.params;
         console.log('request getTournamentImages:', tournamentId, count);
@@ -12,6 +26,23 @@ const tournamentController = {
         
         res.json(imageUrls);
     },
+
+    async getTournamentRanking(req, res) {
+        const { tournamentId } = req.params;
+        console.log('request getTournamentRanking:', tournamentId);
+
+        const tournamentRanking = await tournamentService.getTournamentRanking(tournamentId);
+
+        res.json(tournamentRanking);
+    },
+
+    async updateTournamentWinner(req, res) {
+        const { winnerId } = req.params;
+        console.log('request updateTournamentWinner', winnerId);
+        const result = await tournamentService.updateTournamentWinner(winnerId);
+
+        res.json(result);
+    }
 }
 
 module.exports = tournamentController;
--- src/Routes/TournamentRoute.js
+++ src/Routes/TournamentRoute.js
@@ -2,6 +2,10 @@ const express = require('express');
 const tournamentController = require('../Controllers/TournamentController');
 const router = express.Router();
 
+router.get('/all', tournamentController.getTournaments);
+router.get('/counts/:tournamentId', tournamentController.getTournamentCounts);
 router.get('/images/:tournamentId/:count', tournamentController.getTournamentImages);
+router.get('/ranking/:tournamentId', tournamentController.getTournamentRanking);
+router.get('/winner/:winnerId', tournamentController.updateTournamentWinner);
 
 module.exports = router;
