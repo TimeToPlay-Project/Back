5b006af46c34249b5576731e736d1d55cdeb9a83
sk-jo <gajeong01@gmail.com>
2024-12-31 15:11:26 +0900

    connect

--- src/Config/db.js
+++ src/Config/db.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',     
-    password: 'root',  
+    host: 'database-1.c9uui8asw6eb.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',     
+    password: 'timetoplay',  
     database: 'TTP'   
 });
 
